49188ef94961763389e16665d5fab6852504a474
paulmunyao <paulmunyao094@gmail.com>
2022-08-30 18:08:40 +0300

    add url path for api's

--- YOYO/urls.py
+++ YOYO/urls.py
@@ -19,5 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('yoyobackend.urls'))
-    
+    path('api-auth/', include('rest_framework.urls'))
 ]
