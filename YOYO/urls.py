036afb4d3e1ab950ec25d4fa827c0abd91b09196
paulmunyao <paulmunyao094@gmail.com>
2022-08-30 18:09:49 +0300

    add a comma to remove error

--- YOYO/urls.py
+++ YOYO/urls.py
@@ -18,6 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('yoyobackend.urls'))
+    path('',include('yoyobackend.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
