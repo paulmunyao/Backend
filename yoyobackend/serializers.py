035751b9836fc9dd5c584bb7741af6bd0f1714f1
paulmunyao <paulmunyao094@gmail.com>
2022-09-01 23:35:49 +0300

    indent code

--- yoyobackend/serializers.py
+++ yoyobackend/serializers.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User,Group
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-      model = User 
-       fileds = ['url']
+        model = User
+        fileds = ['url']
