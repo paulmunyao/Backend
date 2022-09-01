3942a2abb07fbac75f668ddc5a0fcbe662ba2f08
paulmunyao <paulmunyao094@gmail.com>
2022-09-01 16:02:30 +0300

    add default permission classes

--- YOYO/settings.py
+++ YOYO/settings.py
@@ -51,7 +51,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = 'YOYO.urls'
 
