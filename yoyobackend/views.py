30d0670d42b43ee66965b471617cc980088206c9
paulmunyao <paulmunyao094@gmail.com>
2022-09-26 23:56:14 +0300

    edit response

--- yoyobackend/views.py
+++ yoyobackend/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 import requests
 # Create your views here.
-r = requests.get('https://api.github.com/users/naveenkrnl')
+response = requests.get('https://api.github.com/users/naveenkrnl')
 
 print(r)
 print(r.content)
